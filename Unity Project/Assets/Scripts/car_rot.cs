e26397cf46e93bd8aa3debf60cbfe366449af550
AOjemann <robintood@hotmail.com>
2014-09-28 15:09:11 -0400

    prep for shoot integration
    getting rdy to combine with shooting

--- Unity Project/Assets/Scripts/car_rot.cs
+++ Unity Project/Assets/Scripts/car_rot.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class car_rot : MonoBehaviour {
 
+	public float sens = 10.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,7 +13,7 @@ public class car_rot : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//rotates the player using the right stick
-		transform.Rotate(0, Input.GetAxis("carrot") /* * sensitivityX */, 0);
+		transform.Rotate(0, Input.GetAxis("carrot") * sens/* * sensitivityX */, 0);
 	
 	}
 }
