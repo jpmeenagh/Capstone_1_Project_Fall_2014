11f785a1e6e5e80d9f296f495c7aacf72f112e54
AOjemann <robintood@hotmail.com>
2014-09-28 14:39:31 -0400

    world move and rotation
    dual stick shooter style controls

---
+++ Unity Project/Assets/Scripts/car_rot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class car_rot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rotates the player using the right stick
+		transform.Rotate(0, Input.GetAxis("carrot") /* * sensitivityX */, 0);
+	
+	}
+}
