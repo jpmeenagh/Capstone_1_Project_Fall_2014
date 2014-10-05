508d5c40e52f4af9ef0e0150970fb647681341d0
AOjemann <robintood@hotmail.com>
2014-10-05 17:35:44 -0400

    bubble shield -player soot
    bubble shield implemented except for blocking player shots

---
+++ Unity Project/Assets/Scripts/abil/bb_shi.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bb_shi : MonoBehaviour {
+
+	public string bb_shiKey = "Fire3";
+	public Transform player;
+	public Rigidbody shi;
+	public int shieldCD = 100;
+	int shieldTime = 0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetButtonDown (bb_shiKey))&& shieldTime == 0) {
+			layShi_bb();
+				}
+		if (shieldTime > 0){
+			shieldTime = shieldTime + 1;
+		}
+		if (shieldTime > shieldCD) {
+			shieldTime = 0;
+				}
+	}
+
+	void layShi_bb(){
+		Instantiate (shi, new Vector3(player.position.x,player.position.y,player.position.z), player.rotation);
+		shieldTime = 1;
+		}
+}
