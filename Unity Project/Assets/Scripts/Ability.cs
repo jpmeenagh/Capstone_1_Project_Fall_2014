5b0bdfb247103eeb9fee332e2b7144441201b111
obrien1jj <obrien1jj@gmail.com>
2014-10-17 15:36:57 -0400

    started working on ability stuff

---
+++ Unity Project/Assets/Scripts/Ability.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Ability : MonoBehaviour {
+	//this is the cooldown on the ability
+	public int max_cooldown;
+
+	//stores which method should be called from the stance
+	public delegate void stance_delegate();
+
+	// Use this for initialization
+	void Start (int given_max_cooldown) {
+		this.max_cooldown = given_max_cooldown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void basic(){}
+	public void attack(){}
+	public void defend(){}
+	public void support(){}
+}
---
+++ Unity Project/Assets/Scripts/Ability_Flamethrower.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ability_Flamethrower : Ability {
+
+	// Use this for initialization
+	//void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
