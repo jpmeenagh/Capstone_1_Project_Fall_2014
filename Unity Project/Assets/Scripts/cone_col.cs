4781649b30b30f42bf54c2520f0befc3e6d24e2f
AOjemann <robintood@hotmail.com>
2014-10-24 17:24:10 -0400

    working on cone collider

---
+++ Unity Project/Assets/Scripts/cone_col.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cone_col : MonoBehaviour {
+
+	public int lifeTime = 100;
+	int scaleTracker;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		lifeTime = lifeTime - 1;
+		if (lifeTime == 0) {
+			//this.
+				}
+		this.transform.localScale = new Vector3(this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z + 1.00f);
+		//scaleTracker = scaleTracker + 1;
+		//this.transform.localScale.z = scaleTracker;
+	}
+}
