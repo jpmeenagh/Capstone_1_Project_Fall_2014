88991b72cb29bbf7acb3e2804db2ade40dfbd040
AOjemann <robintood@hotmail.com>
2014-10-26 16:00:14 -0400

    sabo and cone

--- Unity Project/Assets/Scripts/Ability_Sabotage.cs
+++ Unity Project/Assets/Scripts/Ability_Sabotage.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class Ability_Sabotage : Ability {
 
+	/*
+	 * relevent for flamethrower
+	int coneSpamTime;
+	int coneSpamCount;
+	*/
+	public GameObject cone_colider;
+	GameObject cone_aoe;
+
+	public int saboStr;
+	public int sabotDur;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,11 +25,15 @@ public class Ability_Sabotage : Ability {
 	}
 
 	protected override void attack(){
-		
+		cone_aoe = (GameObject)Instantiate (cone_colider, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), this.transform.rotation);
+		cone_col cone_col_tmp = cone_aoe.GetComponent<cone_col>();
+		cone_col_tmp.setPerams (saboStr, sabotDur, 1.0f);
 	}
 
 	protected override void defend(){
-		
+		cone_aoe = (GameObject)Instantiate (cone_colider, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), this.transform.rotation);
+		cone_col cone_col_tmp = cone_aoe.GetComponent<cone_col>();
+		cone_col_tmp.setPerams (saboStr, sabotDur, 1.1f);
 	}
 
 	protected override void support(){
