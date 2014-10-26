c260fb08da10d65949b883b6c54f7c5122a8dd0b
AOjemann <robintood@hotmail.com>
2014-10-26 19:00:52 -0400

    animations basic

---
+++ Unity Project/Assets/Scripts/mesh_parenting/player_forearm_armor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_forearm_armor : MonoBehaviour {
+
+	public GameObject armBone;
+
+	// Use this for initialization
+	void Start () {
+	//armBone = this
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this.transform = armBone.transform;
+	}
+}
