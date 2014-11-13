8f36ac6f0c62a27d042fe448edc93e19adaa7004
obrien1jj <obrien1jj@gmail.com>
2014-11-13 16:22:08 -0500

    moved healing stuff into the heal ability script

--- Unity Project/Assets/Scripts/Ability_Heal.cs
+++ Unity Project/Assets/Scripts/Ability_Heal.cs
@@ -14,13 +14,14 @@ public class Ability_Heal : Ability {
 	
 	//attack stance effect.  Should only be called using this.stance_delegate
 	protected override int attack(){
-		GameObject player_resource = Resources.Load<GameObject>("fire_mine");
+		GameObject player_resource = Resources.Load<GameObject>("missile");
 		Instantiate(player_resource, this.transform.position - (2 * transform.forward), Quaternion.identity);
 		return max_cooldown_attack;
 	}
 	//defend stance effect.  Should only be called using this.stance_delegate
 	protected override int defend(){
-		print ("cake");
+		GameObject station = Resources.Load<GameObject>("fire_mine");
+		Instantiate(station, transform.position, Quaternion.identity);
 		return max_cooldown_defend;
 	}
 	//support stance effect.  Should only be called using this.stance_delegate
